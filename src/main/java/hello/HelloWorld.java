28988cec575b526ae47ea66fe4dca4623fb03060
Rasolomanana Herimanitra Olivier <98014033+OlivierDevMaster@users.noreply.github.com>
2026-05-18 21:41:40 +0300

    test

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,6 +3,7 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
+  // Some changes
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
